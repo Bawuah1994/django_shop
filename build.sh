2d1cb1606ca7403e58bd65a4f5e5809e5dba0f0e
boateng <bonsuboateng@yahoo.com>
2023-05-03 22:59:49 +0100

    edit

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit  # exit on error
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py migrate --run-syncdb
 python manage.py populate_tables
 python manage.py createsu
 python manage.py parse_csv
