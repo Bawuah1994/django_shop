8463d8d97cca2302566e4ad2292c9cfc9d5f26dd
boateng <bonsuboateng@yahoo.com>
2023-05-03 22:45:42 +0100

    build commands modified for deployment

--- build.sh
+++ build.sh
@@ -6,4 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py populate_tables
 python manage.py createsu
+python manage.py parse_csv
